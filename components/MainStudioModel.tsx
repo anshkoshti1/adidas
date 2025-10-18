3da4f63f382a69b47cb4c185d4557c68cc09972b
anshkoshti1 <adkbro4@gmail.com>
2025-10-18 17:55:06 +0530

    Hover Animation on Models added using GSAP

--- components/MainStudioModel.tsx
+++ components/MainStudioModel.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 import { useMainStudioTextures } from "@/lib/useTextures";
 import { createMaterial } from "@/lib/materials";
 import { studioTextures } from "@/lib/textures";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 type GLTFResult = {
   nodes: {
@@ -22,35 +24,97 @@ export function MainStudioModel() {
     THREE.MeshBasicMaterial
   >;
 
+  const shirts = [
+    {
+      position: [0.65, 0.7, -0.45] as [number, number, number],
+      rotation: [0, Math.PI / 9, 0] as [number, number, number],
+      geometry: nodes.Shirt_White.geometry,
+      material: materials.whiteShirt,
+      hoverMaterial: materials.whiteStudio,
+      slug: "white",
+    },
+    {
+      position: [0, 0.7, 0] as [number, number, number],
+      rotation: [0, 0, 0] as [number, number, number],
+      geometry: nodes.Shirt_Sport.geometry,
+      material: materials.sportShirt,
+      hoverMaterial: materials.redStudio,
+      slug: "sport",
+    },
+    {
+      position: [-0.65, 0.7, -0.45] as [number, number, number],
+      rotation: [0, -Math.PI / 9, 0] as [number, number, number],
+      geometry: nodes.Shirt_Gray.geometry,
+      material: materials.grayShirt,
+      hoverMaterial: materials.grayStudio,
+      slug: "gray",
+    },
+  ];
+
+  const [envMaterial, setEnvMaterial] = useState<THREE.MeshBasicMaterial>(
+    materials.defaultStudio
+  );
+  const meshRefs = useRef<(THREE.Mesh | null)[]>([]);
+  const tlRefs = useRef<GSAPTimeline[]>([]);
+  useGSAP(() => {
+    if (!meshRefs.current) return;
+    meshRefs.current.forEach((mesh, index) => {
+      if (!mesh) return;
+      tlRefs.current[index] = gsap
+        .timeline({ paused: true })
+        .to(mesh.rotation, { y: 0, duration: 0.5, ease: "power1.inOut" }, "<")
+        .to(
+          mesh.scale,
+          {
+            x: 1.05,
+            y: 1.05,
+            z: 1.05,
+            duration: 0.5,
+            ease: "power1.inOut",
+          },
+          "<"
+        );
+    });
+  });
+
+  function enterHandler(index: number, material: THREE.MeshBasicMaterial) {
+    document.body.style.cursor = "pointer";
+    setEnvMaterial(material);
+    tlRefs.current[index].play();
+  }
+
+  function leaveHandler(index: number) {
+    document.body.style.cursor = "auto";
+    tlRefs.current[index].reverse();
+  }
   return (
     <group dispose={null}>
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Environment.geometry}
-        material={materials.defaultStudio}
+        material={envMaterial}
       />
-      <mesh
-        geometry={nodes.Shirt_White.geometry}
-        position={[0.65, 0.7, -0.45]}
-        rotation={[0, Math.PI / 9, 0]}
-        material={materials.whiteShirt}
-      />
-      <mesh
-        geometry={nodes.Shirt_Sport.geometry}
-        position={[0, 0.7, 0]}
-        material={materials.sportShirt}
-      />
-      <mesh
-        geometry={nodes.Shirt_Gray.geometry}
-        position={[-0.65, 0.7, -0.45]}
-        rotation={[0, -Math.PI / 9, 0]}
-        material={materials.grayShirt}
-      />
+      {shirts.map((shirt, i) => (
+        <mesh
+          key={i}
+          ref={(m) => {
+            if (!m) return;
+            meshRefs.current[i] = m;
+          }}
+          geometry={shirt.geometry}
+          material={shirt.material}
+          position={shirt.position}
+          rotation={shirt.rotation}
+          onPointerEnter={() => enterHandler(i, shirt.hoverMaterial)}
+          onPointerLeave={() => leaveHandler(i)}
+        />
+      ))}
       <mesh
         geometry={nodes.Hitbox.geometry}
         scale={[2.52, 1, 1]}
         visible={false}
+        onPointerLeave={() => setEnvMaterial(materials.defaultStudio)}
       />
     </group>
   );
